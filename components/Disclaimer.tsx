61e5e075cc1f807691acfb420d17cb2b778a878a
Nikos D <nikossoftwaredev@gmail.com>
2024-03-21 03:59:13 +0200

    added iframe

--- components/Disclaimer.tsx
+++ components/Disclaimer.tsx
@@ -3,11 +3,17 @@ import { TOKEN_SYMBOL } from "@/data/config";
 
 export const Disclaimer = () => {
   return (
-    <section className="w-full flex-col flex items-center justify-center text-xs">
-      Disclaimer: Cryptocurrency may be unregulated in your jurisdiction. The
-      value of cryptocurrencies may go down as well as up. Profits may be
-      subject to capital gains or other taxes applicable in your jurisdiction.
-    </section>
+    <>
+      <iframe
+        className="w-full h-[500px] col-span-2"
+        src="https://dexscreener.com/solana/3ez5Giu1U9i9kB9U88sC72UmqxYqgqdoizkqmADHodr6?embed=1&theme=dark"
+      ></iframe>
+      <section className="w-full flex-col flex items-center justify-center text-xs">
+        Disclaimer: Cryptocurrency may be unregulated in your jurisdiction. The
+        value of cryptocurrencies may go down as well as up. Profits may be
+        subject to capital gains or other taxes applicable in your jurisdiction.
+      </section>
+    </>
   );
 };
 export default Disclaimer;
