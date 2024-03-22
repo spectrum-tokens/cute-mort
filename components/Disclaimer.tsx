298a76cf931bd64d0a6220b0c34891aeebc1da68
Nikos D <nikossoftwaredev@gmail.com>
2024-03-22 19:20:50 +0200

    added email

--- components/Disclaimer.tsx
+++ components/Disclaimer.tsx
@@ -12,6 +12,8 @@ export const Disclaimer = () => {
         Disclaimer: Cryptocurrency may be unregulated in your jurisdiction. The
         value of cryptocurrencies may go down as well as up. Profits may be
         subject to capital gains or other taxes applicable in your jurisdiction.
+        <br />
+        Sire Creator: nikossoftwaredev@gmail.com
       </section>
     </>
   );
